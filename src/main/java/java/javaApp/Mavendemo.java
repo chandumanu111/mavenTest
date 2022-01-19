188cf9d732672ba72b47044c8d488983b331730d
manuchandu111 <manasakrishna121@gmail.com>
2022-01-20 00:36:43 +0530

    first commit

---
+++ src/main/java/java/javaApp/Mavendemo.java
@@ -0,0 +1,15 @@
+package java.javaApp;
+
+public class Mavendemo {
+	public String Sample()
+	{
+		return "sample";
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Hello world");
+		System.out.println("welcome to maven");
+		
+	}
+
+}
---
+++ src/test/java/java/javaApp/mavendemotest.java
@@ -0,0 +1,14 @@
+package java.javaApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mavendemotest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
